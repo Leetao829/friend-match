4ee23f6ff6fb941b7c817e025b15c43429a5ca1f
liberatad <2002010214@s.upc.edu.cn>
2024-03-09 22:48:14 +0800

    提取推荐用户redis键常量

---
+++ src/main/java/com/leetao/usercenter/constant/RedisConstant.java
@@ -0,0 +1,14 @@
+package com.leetao.usercenter.constant;
+
+/**
+ * redis常数
+ *
+ * @author leetao
+ */
+public interface RedisConstant {
+
+	/**
+	 * 保存在推荐伙伴中的key值
+	 */
+	String RECOMMEND_KEY = "yupao:user:recommend:";
+}
